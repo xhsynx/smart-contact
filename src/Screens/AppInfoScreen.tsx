ad1d60412757423874346f38a2e73879c4151794
xhsynx <huseyin.akcicek@outlook.com>
2020-03-03 00:21:05 +0300

    Update AppInfoScreen.tsx

--- src/Screens/AppInfoScreen.tsx
+++ src/Screens/AppInfoScreen.tsx
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from "react-native";
 export default function AppInfoScreen() {
   return (
     <View style={styles.container}>
-      <Text style={{ color: "#2e7d32" }}>App info screen</Text>
+      <Text style={{ fontSize: 50 }}>😊</Text>
+      <Text style={{ color: "#2e7d32" }}>Download GitHub Link</Text>
+      <Text style={{ fontSize: 50 }}>⤵️</Text>
+      <Text style={{ color: "#2e7d32" }}>
+        https://github.com/xhsynx/smart-contact
+      </Text>
     </View>
   );
 }
